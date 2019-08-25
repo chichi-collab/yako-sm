69f69a6d8ddc2411c45943fc688bf422ed4c26cc
king-kloy <kingkloy16@gmail.com>
2019-08-25 20:29:35 +0000

    add notice board view

---
+++ src/views/NoticeBoard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="all-teachers">
+    <p>Notice Board</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "noticeBoard"  
+};
+</script>
+
+<style scoped></style>
