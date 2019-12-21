44ea6059a9e59451d57d91c22699472a796e8cdc
Okai Yeboah <kingkloy16@gmail.com>
2019-12-21 22:05:26 +0000

    add fees-expense data and chart

---
+++ src/components/LineChart.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about">
+    <canvas id="school-population-chart"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from "chart.js";
+
+// school population data
+import schoolPopulationData from "../../models/data/school-population-data";
+
+export default {
+  mounted() {
+    this.createChart("school-population-chart", this.schoolPopulationData);
+  },
+  methods: {
+    createChart(chartId, chartData) {
+      const ctx = document.getElementById(chartId);
+
+      const myChart = new Chart(ctx, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options
+      });
+
+      console.log(myChart);
+    }
+  },
+  data() {
+    return {
+      schoolPopulationData
+    };
+  }
+};
+</script>
