42df7e5a8f1a18c3a6fd29039c796ad88c3f90a3
Okai Yeboah <kingkloy16@gmail.com>
2019-12-25 20:19:23 +0000

    add run, get and all funtions for database

--- models/database/database-connection.js
+++ models/database/database-connection.js
@@ -1,5 +1,5 @@
 import Sqlite3 from "sqlite3";
-// const Promise = require("bluebird");
+import Promise from "bluebird";
 
 /**
  * @class DatabaseConnection connects to a Sqlite3 database file
@@ -11,12 +11,58 @@ class DatabaseConnection {
     const databaseFilePath = "./yako-sm-database"; // database file
 
     // databaseConn stores the connection
-    this.databaseConn = Sqlite3.Database(databaseFilePath, error => {
+    this.databaseConn = new Sqlite3.Database(databaseFilePath, error => {
       if (error) {
         console.warn("[-] Error: " + error);
       } else {
         console.log("[+] Database connected successfully");
       }
+    });
+  }
+
+  // runs query successfully and asynchronous
+  // otherwise returns -1 as error status
+  run(query, params = []) {
+    return new Promise((resolve, reject) => {
+      this.databaseConn.run(query, params, (error, result) => {
+        if (error) {
+          console.log("[-] Error: error occurred running '" + query + "'");
+          console.log(error);
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  // fetch a single row from the specified table asynchronously
+  get(query, params = []) {
+    return new Promise((resolve, reject) => {
+      this.databaseConn.get(query, params, (error, result) => {
+        if (error) {
+          console.log("[-] Error: error occurred running '" + query + "'");
+          console.log(error);
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  // fetch all rows from the specified table
+  all(query, params = []) {
+    return new Promise((resolve, reject) => {
+      this.databaseConn.all(query, params, (error, rows) => {
+        if (error) {
+          console.log("[-] Error: error occurred running '" + query + "'");
+          console.log(error);
+          reject(error);
+        } else {
+          resolve(rows);
+        }
+      });
     });
   }
 }
