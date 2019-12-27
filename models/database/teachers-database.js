86244e2c9af07c51a05d68ad5cba1354d1f18017
Okai Yeboah <kingkloy16@gmail.com>
2019-12-27 15:02:54 +0000

    rewrite the CRUD operations with nedb

--- models/database/teachers-database.js
+++ models/database/teachers-database.js
@@ -1,95 +1,131 @@
+import path from "path";
+import Datastore from "nedb";
+import Promise from "bluebird";
+
 /**
  * @class TeachersDatabase CRUD operations on the Teachers table
- * @constructor constructor initialise a connection to the database
+ * @constructor constructor initializes a connection to the database
  *
  */
 class TeachersDatabase {
-  constructor(databaseConn) {
-    // databaseConn stores the connection
-    this.databaseConn = databaseConn;
+  constructor() {
+    // teachers database file path
+    let databasePath = path.resolve(__dirname, "./teachers-table.db");
+
+    // initializes database with the database file path
+    this.database = new Datastore({
+      filename: databasePath
+    });
+
+    // load database to memory
+    this.database.loadDatabase(error => {
+      if (error) {
+        console.log("[-] Error: " + error);
+      }
+    });
   }
 
   // fetch all teachers in the database
   fetchAll() {
-    return this.databaseConn.all("SELECT * FROM teachers");
+    return new Promise((resolve, reject) => {
+      this.database.find({}, (error, result) => {
+        if (error) {
+          console.log("[-] Error: failed to fetch all teachers");
+          reject(error);
+        }
+
+        resolve(result);
+      });
+    });
   }
 
   // fetch teacher by id
-  fetchOne(id) {
-    return this.databaseConn.get(
-      "SELECT * FROM teachers WHERE teacher_id = ?",
-      [id]
-    );
+  fetchOne(_id) {
+    return new Promise((resolve, reject) => {
+      this.database.findOne(
+        {
+          _id
+        },
+        (error, result) => {
+          if (error) {
+            console.log("[-] Error: failed to fetch teacher by id");
+            reject(error);
+          }
+
+          resolve(result);
+        }
+      );
+    });
   }
 
-  // update teacher details where id equals teacher_id
+  // update teacher details where id equals
+  // param teacher contains an object of the teacher details
   update(teacher) {
-    const {
-      teacherId,
-      firstName,
-      lastName,
-      classroom,
-      gender,
-      birthDate,
-      email,
-      phoneNumber,
-      isHeadTutor
-    } = teacher;
+    const { _id } = teacher;
 
-    return this.databaseConn.run(
-      "UPDATE Teachers SET first_name = ?, last_name = ?, classroom = ?, gender = ?, birth_date= ?, email = ?, phone_number = ?, is_head_tutor = ? WHERE id = ?",
-      [
-        firstName,
-        lastName,
-        classroom,
-        gender,
-        birthDate,
-        email,
-        phoneNumber,
-        isHeadTutor,
-        teacherId
-      ]
-    );
+    return new Promise((resolve, reject) => {
+      this.database.update(
+        {
+          _id
+        },
+        teacher,
+        {},
+        (error, numReplaced) => {
+          if (error) {
+            console.log("[-] Error: failed to fetch teacher by id");
+            reject(error);
+          } else if (numReplaced == 1) {
+            resolve("update teacher details successfully");
+          }
+        }
+      );
+    });
   }
 
   // add teacher to the database
+  // param teacher contains object of teacher details
   add(teacher) {
-    const {
-      teacherId,
-      firstName,
-      lastName,
-      classroom,
-      gender,
-      birthDate,
-      email,
-      phoneNumber,
-      isHeadTutor
-    } = teacher;
-
-    return this.databaseConn.run(
-      "INSERT INTO Teachers(teacher_id, first_name, last_name, classroom, gender, birth_date,  email, phone_number, is_head_tutor) VALUES( ? , ? , ? , ? , ? , ? , ? , ? , ? , ? );",
-      [
-        teacherId,
-        firstName,
-        lastName,
-        classroom,
-        gender,
-        birthDate,
-        email,
-        phoneNumber,
-        isHeadTutor
-      ]
-    );
+    return new Promise((resolve, reject) => {
+      this.database.insert(teacher, (error, result) => {
+        if (error) {
+          console.log("[-] Error: failed to add new teacher");
+          reject(error);
+        }
+        resolve(result);
+      });
+    });
   }
 
   // delete teacher from the database
-  delete(teacher) {
-    const { teacherId } = teacher;
+  // param _id the unique id by the teacher
+  delete(_id) {
+    return new Promise((resolve, reject) => {
+      this.database.remove(
+        {
+          _id
+        },
+        {},
+        (error, result) => {
+          if (error) {
+            console.log("[-] Error: failed to delete teacher");
+            reject(error);
+          }
 
-    return this.databaseConn.run("DELETE FROM Teachers WHERE id = ?", [
-      teacherId
-    ]);
+          resolve(result);
+        }
+      );
+    });
   }
 }
 
 export default TeachersDatabase;
+
+//   id: 1,
+//   firstName: "Okai",
+//   lastName: "Kloy",
+//   classroom: "JHS 1",
+//   gender: "Male",
+//   birthDate: "",
+//   email: "",
+//   contact: "",
+//   isHeadTutor: true
