83daeea6d2676092de31ee5e4428d8d4b83aad98
Okai Yeboah <kingkloy16@gmail.com>
2020-03-19 20:19:37 +0000

    fix expenses_id mismatch issue

--- src/models/database/expense-table.js
+++ src/models/database/expense-table.js
@@ -15,15 +15,15 @@ class ExpensesTable {
 
   // fetch expense by id
   fetchOne(id) {
-    return this.database.get(`SELECT * FROM Expenses WHERE expense_id = ?`, [
+    return this.database.get(`SELECT * FROM Expenses WHERE expenses_id = ?`, [
       id
     ]);
   }
 
-  // update expense details where id equals expense_id
+  // update expense details where id equals expenses_id
   update(expense) {
     const {
-      expenseId,
+      expensesId,
       expenseType,
       name,
       status,
@@ -40,8 +40,16 @@ class ExpensesTable {
         amount_taken = ?,
         reason = ?,
         date_of_expense = ?
-      WHERE expense_id = ?`,
-      [expenseType, name, status, amountTaken, reason, dateOfExpense, expenseId]
+      WHERE expenses_id = ?`,
+      [
+        expenseType,
+        name,
+        status,
+        amountTaken,
+        reason,
+        dateOfExpense,
+        expensesId
+      ]
     );
   }
 
@@ -73,13 +81,15 @@ class ExpensesTable {
   // delete expense from the database
   // param d the unique id by the expense
   delete(id) {
-    return this.database.run(`DELETE FROM Expenses WHERE expense_id = ?`, [id]);
+    return this.database.run(`DELETE FROM Expenses WHERE expenses_id = ?`, [
+      id
+    ]);
   }
 }
 
 export default ExpensesTable;
 
-//   expenseId: ,
+//   expensesId: ,
 //   expenseType: "",
 //   name: "",
 //   status: "",
